98b89dcf28fc1f080a855da26a8c061be6e494f2
Mia <tamiapaschall@gmail.com>
2019-11-23 02:27:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(speak)
   if speak == speak.upcase
-   return "NO, NOT SINCE 1938!"
+   return "I LOVE YOU TOO PUMPKIN!"
  elsif speak == "I LOVE YOU GRANDMA!"
- return "I LOVE YOU TOO PUMPKIN!"
+ return "NO, NOT SINCE 1938!"
   else
    return "HUH?! SPEAK UP, SONNY!"
   end
